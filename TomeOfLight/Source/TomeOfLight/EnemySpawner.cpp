620217c1d99250b02a5472715b495e40d3e3baa2
fedepvg <federicopvg@gmail.com>
2020-06-26 17:59:48 -0300

    enemy spawner

---
+++ TomeOfLight/Source/TomeOfLight/EnemySpawner.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemySpawner.h"
+#include "Components/BoxComponent.h"
+#include "Engine/TriggerBox.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "ElementalPawn.h"
+
+// Sets default values
+AEnemySpawner::AEnemySpawner()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AEnemySpawner::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorld()->GetTimerManager().SetTimer(SpawnTimer, this, &ThisClass::SpawnEnemy, .5f, true);
+}
+
+void AEnemySpawner::SpawnEnemy()
+{
+	int SpawnAreaIndex = FMath::RandRange(0, SpawnAreas.Max()-1);
+	FVector SpawnOrigin = SpawnAreas[SpawnAreaIndex]->GetCollisionComponent()->Bounds.Origin;
+	FVector SpawnExtent = SpawnAreas[SpawnAreaIndex]->GetCollisionComponent()->Bounds.BoxExtent;
+	FVector SpawnPoint = UKismetMathLibrary::RandomPointInBoundingBox(SpawnOrigin, SpawnExtent);
+
+	AActor* SpawnedActor = GetWorld()->SpawnActor(EnemyClass, &SpawnPoint);
+	AElementalPawn* Enemy = Cast<AElementalPawn>(SpawnedActor);
+	if(Enemy != nullptr)
+	{
+		Enemy->SetMovementVector((PlayerReference->GetActorLocation() - SpawnPoint).GetSafeNormal());
+	}
+}
+
+// Called every frame
+void AEnemySpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ TomeOfLight/Source/TomeOfLight/EnemySpawner.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TimerManager.h"
+#include "EnemySpawner.generated.h"
+
+UCLASS()
+class TOMEOFLIGHT_API AEnemySpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEnemySpawner();
+
+protected:
+	UPROPERTY(EditAnywhere)
+	AActor* PlayerReference;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AActor> EnemyClass;
+
+	UPROPERTY(EditAnywhere)
+	TArray<class ATriggerBox*> SpawnAreas;
+
+	FTimerHandle SpawnTimer;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void SpawnEnemy();
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
